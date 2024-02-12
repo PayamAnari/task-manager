9ff1794041c7901e24d7e1a4abc113a5183addd7
PayamAnari <anari.p62@gmail.com>
2024-02-12 22:23:34 +0100

    Modify task controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,10 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
-
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index(request $request)
+    {
+
+        return response()->json(Task::all());
+    }
 }
