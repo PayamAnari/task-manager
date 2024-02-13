405e5e2cb3c6acb8790adc79c6af10c7fe9a2b3b
PayamAnari <anari.p62@gmail.com>
2024-02-13 14:19:16 +0100

    Add allowed sort

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,21 +8,25 @@ use App\Http\Resources\TaskCollection;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class TaskController extends Controller
 {
     public function index(request $request)
     {
+        $tasks = QueryBuilder::for(Task::class)
+            ->allowedFilters('is_done')
+            ->defaultSorts('created_at')
+            ->allowedSorts('title', 'is_done', 'created_at')
+            ->paginate();
 
-        return new TaskCollection(Task::paginate());
+        return new TaskCollection($tasks);
     }
 
     public function show(Request $request, Task $task)
     {
-        $tasks = QueryBuilder::for(Task::class)
-            ->allowedFilters('is_done')
-            ->paginate();
-        return new TaskResource($tasks);
+
+        return new TaskResource($task);
     }
 
     public function store(StoreTaskRequest $request)
