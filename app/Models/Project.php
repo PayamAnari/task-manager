706485056fd9453184a99f1ed1fd1bdd5ef01e64
PayamAnari <anari.p62@gmail.com>
2024-02-14 14:57:08 +0100

    Modify project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title'];
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function creator(): HasMany
+    {
+        return $this->belongsTo(User::class, 'creator_id');
+    }
 }
