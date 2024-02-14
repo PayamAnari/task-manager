e6ebdffef8200e313ff3084352ec20bbfc12cbba
PayamAnari <anari.p62@gmail.com>
2024-02-14 14:26:33 +0100

    Create register func in auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,4 +27,21 @@ class AuthController extends Controller
             'token_type' => 'Bearer',
         ]);
     }
+
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|max:225|email|unique:users,email',
+            'password' => 'required|confirmed|max:6',
+        ]);
+
+        $user = User::create($validated);
+
+        return response()->json([
+            'data' => $user,
+            'access_token' => $user->createToken('api_token')->plainTextToken,
+            'token_type' => 'Bearer',
+        ]);
+    }
 }
