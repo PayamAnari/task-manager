b3711965ef9853bf51c1848122236286e16281f2
PayamAnari <anari.p62@gmail.com>
2024-02-15 12:31:33 +0100

    Create store func in project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Request\UpdateProjectRequest;
+use App\Http\Resources\ProjectResource;
+use App\Models\Project;
+use Illuminate\Support\Facades\Auth;
 
 class ProjectController extends Controller
 {
-    //
+    public function store(StoreProjectRequest $request)
+    {
+        $validated = $request->validated();
+
+        $project = Auth::user()->projects()->create($validated);
+
+        return new ProjectResource($project);
+    }
+
+    public function update(UpdateProjectRequest $request, Project $project)
+    {
+        $project = Project::findOrFail($request->id);
+        $project->update($request->validated());
+
+        return new ProjectResource($project);
+    }
 }
