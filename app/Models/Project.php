578701148e3d755b7eac1f289622d087aa6380c9
PayamAnari <anari.p62@gmail.com>
2024-02-15 13:26:55 +0100

    Create projectcollection resource

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use App\Models\Task;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Auth;
 
 class Project extends Model
 {
